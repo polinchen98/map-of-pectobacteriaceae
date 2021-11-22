2a9eaa90c6ed93c51be1cf7c04b7346c1ab41249
Polina Rasskazova <rasskazova.pm@phystech.edu>
2021-11-22 11:13:17 +0300

    Update .gitignore

--- app_map/about.R
+++
@@ -1,5 +0,0 @@
-about <- '<h1>Map of location Pectobacteriaceae</h1>
-<p>On the first page there is the map with locatin of finding bacterias from family <b>Pectobacteriaceae</b>. 
-On the second page there is plot with event date of bacterias and number of finding in each year.
-</p>
-'
